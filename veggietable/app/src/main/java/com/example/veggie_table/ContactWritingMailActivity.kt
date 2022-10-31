b191ad520446fdc45bdacde86e6b8a750b297058
yxxyn20 <dbtndus0527@gmail.com>
2022-11-01 01:06:47 +0900

    액티비티 생성

---
+++ veggietable/app/src/main/java/com/example/veggie_table/ContactWritingMailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.veggie_table
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactWritingMailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_writing_mail)
+    }
+}
