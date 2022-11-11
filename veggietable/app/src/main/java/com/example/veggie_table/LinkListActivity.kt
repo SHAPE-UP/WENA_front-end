29cd52b78d9885bb17c2644df6790013b33c5585
yellowjam <lerle144@gmail.com>
2022-11-11 17:13:02 +0900

    웹사이트 추가

--- veggietable/app/src/main/java/com/example/veggie_table/LinkListActivity.kt
+++ veggietable/app/src/main/java/com/example/veggie_table/LinkListActivity.kt
@@ -1,11 +1,34 @@
 package com.example.veggie_table
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.veggie_table.databinding.ActivityLinkListBinding
+import kotlinx.android.synthetic.main.activity_link_list.*
+
 
 class LinkListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_link_list)
+
+        val binding = ActivityLinkListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.banner1.setOnClickListener {
+            val mIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.assembly.go.kr/portal/main/main.do"))
+            startActivity(mIntent)
+        }
+
+        binding.banner2.setOnClickListener {
+            val mIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://likms.assembly.go.kr/bill/main.do"))
+            startActivity(mIntent)
+        }
+
+        binding.banner3.setOnClickListener {
+            val mIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://petitions.assembly.go.kr/"))
+            startActivity(mIntent)
+        }
+
     }
 }
